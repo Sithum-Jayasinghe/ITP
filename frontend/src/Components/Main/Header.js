0867d50dfed7d493d938b87def114fd6360bbc10
Sithum-Jayasinghe <abishekjayasinghe944@gmail.com>
2025-09-22 07:06:21 +0530

    Added new features / updated code

--- frontend/src/Components/Main/Header.js
+++ frontend/src/Components/Main/Header.js
@@ -7,25 +7,29 @@ const Header = () => {
 
   return (
     <header className="App-header">
-      
-      
+
+
       <div className="button-container">
-      
+
+        <button className="user-button" onClick={() => navigate('/books')}>Booking</button>
         <button className="user-button" onClick={() => navigate('/schedules')}>Shedules</button>
-         <button className="user-button" onClick={() => navigate('/staffs')}>Staff</button>
-         <button className="user-button" onClick={() => navigate('/books')}>Booking</button>
-          <button className="user-button" onClick={() => navigate('/passengers')}>Passenger</button>
-            <button className="user-button" onClick={() => navigate('/checks')}>Checking</button>
-              <button className="user-button" onClick={() => navigate('/payments')}>Payments</button>
-           <button className="user-button" onClick={() => navigate('/registers')}>Register</button>
-   
+        <button className="user-button" onClick={() => navigate('/passengers')}>Passenger</button>
+        <button className="user-button" onClick={() => navigate('/checks')}>Checking</button>
+        <button className="user-button" onClick={() => navigate('/payments')}>Payments</button>
+        <button className="user-button" onClick={() => navigate('/staffs')}>Staff</button>
+        <button className="user-button" onClick={() => navigate('/registers')}>Register</button>
 
 
-         
+
+
+
+
+
+
       </div>
 
 
-     
+
     </header>
   );
 };
